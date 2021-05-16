beddc28e3a56b30091f22eb7a00da639aadaed87
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-05-16 14:32:58 +0530

    Added Images and made a large size styling to the mens and womens section

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,9 +50,9 @@ class Directory extends Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title, imageUrl, id}) => {
+                    this.state.sections.map(({title, imageUrl, id, size}) => {
                         return (
-                            <MenuItem key={id} title={title} />
+                            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                         )
                     })
                 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,9 +2,14 @@ import React from "react"
 
 import "./menu-item.styles.scss"
 
-const MenuItem = ({ title }) => {
+const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div className="menu-item">
+        <div style={
+                {
+                    backgroundImage: `url(${imageUrl})`
+                }
+            } className={`${size} menu-item`}
+        >
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
