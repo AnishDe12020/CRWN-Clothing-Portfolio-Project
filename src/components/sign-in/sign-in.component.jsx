b23195ab98a5da8be9c5bebbb73af4b35c613b1a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-04 16:24:34 +0530

    Made form-input component, added styles and changed the sign-in component to use the new component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import FormInput from "../form-input/form-input.component"
+
 import "./sign-in.styles.scss"
 
 class SignIn extends React.Component {
@@ -37,23 +39,23 @@ class SignIn extends React.Component {
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input 
+                    <FormInput 
                         name="email" 
                         type="email"
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
                         value={this.state.email} 
+                        label="email"
                         required 
                     />
-                    <label>Email</label>
 
-                    <input 
+                    <FormInput 
                         name="password"
                         type="password"
-                        onChange={this.handleChange}
+                        handleChange={this.handleChange}
                         value={this.state.password}  
+                        label="password"
                         required
                     />
-                    <label>Password</label>
 
                     <input type="submit" value="Submit Form" />
                 </form>
