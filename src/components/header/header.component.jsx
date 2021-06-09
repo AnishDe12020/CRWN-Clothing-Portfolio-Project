f20e194431a8f481ec20ef5d7cda3e96ef8ffdaa
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-09 15:37:18 +0530

    added redux logic to the header component and made the mapStateToProps function

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { connect } from "react-redux"
 
 import { auth } from "../../firebase/firebase.utils"
 
@@ -30,4 +31,8 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header
+const mapStateToPrope = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToPrope)(Header)
