57e5c3ea1515bd5b4b1603a37b304bc469db0d5d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-19 15:25:38 +0530

    Added shop data to the store, applied redux and reselect logic and made selectors

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,30 +1,23 @@
-import React, { Component } from "react"
+import React from "react"
+import { connect } from "react-redux"
+import { createStructuredSelector } from "reselect"
 
 import CollectionPreview from "../../components/collection-preview/collection-preview.component"
 
-import SHOP_DATA from "./shop.data.js"
+import { selectCollections } from "../../redux/shop/shop.selectors"
 
-class ShopPage extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            collections: SHOP_DATA
+const ShopPage = ({ collections }) => (
+    <div className="shop-page">
+        {
+            collections.map(({ id, ...otherCollectionProps }) => (
+                <CollectionPreview key={id} {...otherCollectionProps} />
+            ))
         }
-    }
+    </div>
+)
 
-    render() {
-        const {collections} = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollections
+})
 
-export default ShopPage
+export default connect(mapStateToProps)(ShopPage)
