3f47780f4fda885d828ade6090d5090d3cad20ea
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-20 15:09:10 +0530

    Made the selectCollection selector and added it to the collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,23 @@
 import React from "react"
+import { connect } from "react-redux"
 
 import CollectionItem from "../../components/collection-item/collection-item.component"
 
+import { selectCollection } from "../../redux/shop/shop.selectors"
+
 import "./collection.styles.scss"
 
-const CollectionPage = ({ match }) => (
+const CollectionPage = ({ collection }) => {
+    console.log(collection)
+    return (
     <div className="collection-page"> 
         <h2>COLLECTION PAGE</h2>
     </div>
-)
+    )
+}
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -6,7 +6,7 @@ import CollectionPage from "../collection/collection.component"
 
 const ShopPage = ({ match }) => (
     <div className="shop-page">
-        <Route exact path={`${match.path}`} comonent={CollectionsOverview} />
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 )
