2a545b3105ed27ead3bbd6b9a0316ccebe6ed7b1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-23 15:22:34 +0530

    Switched to enviromenet variabled only for firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,26 +2,14 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
 
-import fireabseConfig from "./firebase.config.json"
-
-// try {
-//     const fireabseConfig = require("./firebase.config.json")
-// }
-// catch(err) {
-//     console.log("Using ENV config")
-// }
-
-const dotenv = require("dotenv")
-dotenv.config()
-
 const config = {
-    apiKey: process.env.FIREBASE_API_KEY || fireabseConfig.FIREBASE.API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || fireabseConfig.FIREBASE.AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID || fireabseConfig.FIREBASE.PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || fireabseConfig.FIREBASE.STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || fireabseConfig.FIREBASE.MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID || fireabseConfig.FIREBASE.APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || fireabseConfig.FIREBASE.MEASUREMENT_ID
+            apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+            authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.REACT_APP_FIREBASE_APP_ID,
+            measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 }
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
