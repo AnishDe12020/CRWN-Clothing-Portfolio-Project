61cedee70db2a6e5a5143881eaf7f14fb98266d1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-25 14:52:16 +0530

    leveraged styled componened for checkout-item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,26 +3,26 @@ import { connect } from "react-redux"
 
 import { clearItemFromCart, addItem, removeItem } from "../../redux/cart/cart.actions"
 
-import "./checkout-item.styles.scss"
+import { CheckoutItemContainer, ImageContainer, Image, TextContainer, QuantityContainer, RemoveButtonContainer } from "./checkout-item.styles"
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
     const { name, imageUrl, price, quantity } = cartItem
 
     return (
-        <div className="checkout-item">
-            <div className="image-container">
-                <img alt="item" src={imageUrl} />
-            </div>
+        <CheckoutItemContainer>
+            <ImageContainer>
+                <Image alt="item" src={imageUrl} />
+            </ImageContainer>
 
-            <span className="name">{name}</span>
-            <span className="quantity">
+            <TextContainer className="name">{name}</TextContainer>
+            <QuantityContainer>
                 <div className="arrow" onClick={() => removeItem(cartItem)} >&#10094;</div>
                 <span className="value" >{quantity}</span>
                 <div className="arrow" onClick={() => addItem(cartItem)} >&#10095;</div>
-            </span>
-            <span className="price">{price}</span>
-            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
-        </div>
+            </QuantityContainer>
+            <TextContainer>{price}</TextContainer>
+            <RemoveButtonContainer onClick={() => clearItem(cartItem)}>&#10005;</RemoveButtonContainer>
+        </CheckoutItemContainer>
     )
 }
 
