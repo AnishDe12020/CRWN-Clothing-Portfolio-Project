c3ce583243839e8f9ad5d219072dcb32e2bc8ed0
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-25 14:40:04 +0530

    leveraged stylec component for cart-item

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,17 @@
 import React from "react"
 
-import "./cart-item.styles.scss"
+import { CartItemContainer, ItemDetailsContainer, ItemName, CartItemImage } from "./cart-item.styles"
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-    <div className="cart-item">
-        <img src={imageUrl} alt="item" />
-        <div className="item-details">
-            <span className="name">{name}</span>
+    <CartItemContainer>
+    <CartItemImage src={imageUrl} alt="item" />
+        <ItemDetailsContainer>
+            <ItemName>{name}</ItemName>
             <span className="price">
                 {quantity} x ${price}
             </span>
-        </div>
-    </div>
+        </ItemDetailsContainer>
+    </CartItemContainer>
 )
 
 export default CartItem
