5f0bd7905ec797d226d5560afec33235d8ff604f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-26 13:34:30 +0530

    leveraged styled component for checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,45 +7,45 @@ import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selector
 import CheckoutItem from "../../components/checkout-item/checkout-item.component"
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component"
 
-import "./checkout.styles.scss"
+import { CheckoutPageContainer, CheckoutHeaderContainer, CheckoutHeaderBlockContainer, TotalContainer, TestWarningContainer } from "./checkout.styles"
 
 const CheckoutPage = ({ cartItems, total }) => (
-    <div className="checkout-page">
-            <div className="checkout-header">
-                <div className="header-block">
+    <CheckoutPageContainer>
+            <CheckoutHeaderContainer>
+                <CheckoutHeaderBlockContainer>
                     <span>Product</span>
-                </div>
-                <div className="header-block">
+                </CheckoutHeaderBlockContainer>
+                <CheckoutHeaderBlockContainer>
                     <span>Description</span>
-                </div>
-                <div className="header-block">
+                </CheckoutHeaderBlockContainer>
+                <CheckoutHeaderBlockContainer>
                     <span>Price</span>
-                </div>
-                <div className="header-block">
+                </CheckoutHeaderBlockContainer>
+                <CheckoutHeaderBlockContainer>
                     <span>Quantity</span>
-                </div>
-                <div className="header-block">
+                </CheckoutHeaderBlockContainer>
+                <CheckoutHeaderBlockContainer>
                     <span>Remove</span>
-                </div>
-            </div>
+                </CheckoutHeaderBlockContainer>
+            </CheckoutHeaderContainer>
             {
                 cartItems.map(cartItem => 
                     <CheckoutItem key={cartItem.id} cartItem={cartItem} />    
                 )
             }
 
-            <div className="total">
+            <TotalContainer>
                 <span>TOTAL: ${total}</span>
-            </div>
+            </TotalContainer>
 
-            <div className="test-warning">
+            <TestWarningContainer>
                 *Please use the following test credit card for payments*
                 <br />
                 4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
-            </div>
+            </TestWarningContainer>
 
             <StripeCheckoutButton price={total} />
-    </div>
+    </CheckoutPageContainer>
 )
 
 const mapStateTopProps = createStructuredSelector({
