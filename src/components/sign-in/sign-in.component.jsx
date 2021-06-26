a6709512333e44f67db9a487eb0ad2166f74aa25
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-26 13:19:07 +0530

    leveraged styled component for sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import CustomButton from "../custom-button/custom-button.component"
 
 import { auth, signInWithGoogle } from "../../firebase/firebase.utils"
 
-import "./sign-in.styles.scss"
+import { SignInContainer, SignInTitle, ButtonsContainer } from "./sign-in.styles"
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -44,8 +44,8 @@ class SignIn extends React.Component {
 
     render() {
         return (
-            <div className="sign-in">
-                <h2>I already have an account</h2>
+            <SignInContainer>
+                <SignInTitle>I already have an account</SignInTitle>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
@@ -67,12 +67,12 @@ class SignIn extends React.Component {
                         required
                     />
 
-                    <div className="buttons">
+                    <ButtonsContainer>
                         <CustomButton type="submit">Sign in</CustomButton>
                         <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign in with Google</CustomButton>
-                    </div>
+                    </ButtonsContainer>
                 </form>
-            </div>
+            </SignInContainer>
         )
     }
 }
