f1fa9bf9c804c12174f8440157063b881a8fe79b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-26 14:57:02 +0530

    removed the calls for the addCollectionAndDocuments call from App.js because this function was to be fired only once. The function still remains so that it can be fired if needed anytime in the future

--- src/App.js
+++ src/App.js
@@ -12,18 +12,17 @@ import CheckoutPage from "./pages/checkout/checkout.component"
 
 import Header from "./components/header/header.component"
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from "./firebase/firebase.utils"
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils"
 
 import { setCurrentUser } from "./redux/user/user.actions"
 
 import { selectCurrentUser } from "./redux/user/user.selectors"
-import { selectCollectionForPreview } from "./redux/shop/shop.selectors"
 
 class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props
+    const { setCurrentUser } = this.props
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
@@ -39,7 +38,6 @@ class App extends React.Component {
         })
       }
       setCurrentUser(userAuth)
-      addCollectionAndDocuments("collections", collectionsArray.map(({ title, items }) => ({ title, items })))
     })
   }
 
@@ -73,8 +71,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionForPreview
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
