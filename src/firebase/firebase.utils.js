402ed32ee263e1c06a10e76ffcaf6fa99578d7ee
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-27 13:10:40 +0530

    added the collecitons data we retrieved from firebase to our redux state

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -63,7 +63,10 @@ export const convertCollectionsSnapshotToMap = (collections) => {
         }
     })
 
-    console.log(transformedCollection)
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection
+        return accumulator
+    }, {})
 }
 
 firebase.initializeApp(config)
