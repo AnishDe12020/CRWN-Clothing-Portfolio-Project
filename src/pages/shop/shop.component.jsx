45f612187bddde0b0d8a0ed72580d1c51cc89001
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-06-27 14:44:12 +0530

    made the firestore call in the shop component a promise

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -24,11 +24,13 @@ class ShopPage extends React.Component {
         const { updateCollections } = this.props
         const collectionRef = firestore.collection("collections")
 
-        collectionRef.onSnapshot(async snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-            updateCollections(collectionsMap)
-            this.setState({ loading: false })
-        })
+        collectionRef.get().then(
+            snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+                updateCollections(collectionsMap)
+                this.setState({ loading: false })
+            }
+        )   
     }
 
     render() {
