e819657669a48526ef21e4e159a74ce36801a3ab
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-01 16:15:18 +0530

    used redux-thunk to fetch collection data from firestore, moved the firstore collection called to the shop reducer

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -20,3 +20,8 @@ export const selectCollectionForPreview = createSelector(
     [selectCollections],
     collections => (collections ? Object.keys(collections).map(key => collections[key]) : [])
 )
+
+export const selectIsCollectionFetching = createSelector(
+    [selectShop],
+    shop => shop.isFetching
+)
