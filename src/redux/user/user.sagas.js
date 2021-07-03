b990e47cd88042cbacdefec092c39549bf0e5bdf
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-03 15:26:36 +0530

    refactored our code due to similarities between email and google sign in

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,31 +2,38 @@ import { takeLatest, put, all, call } from "redux-saga/effects"
 
 import userActionTypes from "./user.types"
 
-import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from "./user.actions"
+import { SignInSuccess, SignInFailure } from "./user.actions"
 
 import { auth, googleProvider, createUserProfileDocument } from "../../firebase/firebase.utils"
+
+export function* getSnapshotFromUserAuth(userAuth) {
+    try {
+        const userRef = yield call(createUserProfileDocument, userAuth)
+        const userSnapshot = yield userRef.get()
+        yield put(SignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+    } catch(error) {
+        yield put(SignInFailure(error))
+    }
+
+}
 
 export function* signInWithGoogle() {
     try {
         const { user } = yield auth.signInWithPopup(googleProvider)
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get()
-        yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+        yield getSnapshotFromUserAuth(user)
 
     } catch(error) {
-        yield put(googleSignInFailure(error))
+        yield put(SignInFailure(error))
     }
 }
 
 export function* signInWithEmail({payload: { email, password }}) {
     try {
         const { user } = yield auth.signInWithEmailAndPassword(email, password)
-        const userRef = yield call(createUserProfileDocument, user)
-        const userSnapshot = yield userRef.get()
-        yield put(emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
+        yield getSnapshotFromUserAuth(user)
 
     } catch(error) {
-        yield put(emailSignInFailure(error))
+        yield put(SignInFailure(error))
     }
 }
 
