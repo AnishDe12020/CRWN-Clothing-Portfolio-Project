b39936015bd22645acc2aacb0ca8bbc6bcca07a1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-04 14:25:54 +0530

    made a root shopSagas generator function just like the other sagas

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects"
+import { takeLatest, call, put, all } from "redux-saga/effects"
 
 import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils"
 
@@ -10,8 +10,6 @@ import {
 import ShopActionTypes from "./shop.types"
 
 export function* fetchCollectionsAsync() {
-    yield console.log("I am fired")
-
     try {
         const collectionRef = firestore.collection('collections')
         const snapshot = yield collectionRef.get()
@@ -24,4 +22,8 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
     yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync)
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)])
 }
