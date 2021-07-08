363b4d18e04144a403637ae1e07f65275e9056ac
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-08 16:45:59 +0530

    installed the axios library and modified server.js to call the payments api

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from "react"
 import StripeCheckout from "react-stripe-checkout"
+import axios from "axios"
 
 const StripCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_51J56WKSEokNY3wn63iuUiEHjCUAGK9HdPFBKua3W6OiH7G5dt7RQtPoXh2NOmvq9dDny7P6Ve4Xz1ZyohQl6Uz6g00IjBKiDzv"
 
     const onToken = token => {
-        console.log(token)
-        alert("Payment successful")
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+              amount: priceForStripe,
+              token: token
+            }
+          })
+            .then(response => {
+              alert('succesful payment');
+            })
+            .catch(error => {
+              console.log('Payment Error: ', error);
+              alert(
+                'There was an issue with your payment! Please make sure you use the provided credit card.'
+              );
+            });
     }
 
     return (
--- server.js
+++ server.js
@@ -25,18 +25,18 @@ app.listen(port, error => {
     console.log("Server running on port " + port)
 })
 
-app.post("/payment", (req, res) => {
+app.post('/payment', (req, res) => {
     const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: "usd"
-    }
-
+      source: req.body.token.id,
+      amount: req.body.amount,
+      currency: 'usd'
+    };
+  
     stripe.charges.create(body, (stripeErr, stripeRes) => {
-        if(stripeErr) {
-            res.status(500).send({ error: stripeErr })
-        } else {
-            res.status(200).send({ success: stripeRes })
-        }
-    })
-})
+      if (stripeErr) {
+        res.status(500).send({ error: stripeErr });
+      } else {
+        res.status(200).send({ success: stripeRes });
+      }
+    });
+  });
