fa43ba2ba6b1d849e3294f101524b79177e9ab22
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-07-15 16:51:31 +0530

    updated our app to be pwa compliant, added service-worker, https and updates manifest

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,8 @@ import App from './App'
 
 import { store, persistor } from "./redux/store"
 
+import * as serviceWorker from "./service-worker"
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -19,4 +21,6 @@ ReactDOM.render(
   </Provider>
   ,
   document.getElementById('root')
-);
+)
+
+serviceWorker.register()
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path")
 const compression = require("compression")
+const enforce = require("express-sslify")
 
 if(process.env.NODE_ENV !== "production") require("dotenv").config()
 
@@ -13,6 +14,7 @@ const port = process.env.PORT || 5000
 app.use(compression())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "client/build")))
@@ -27,12 +29,16 @@ app.listen(port, error => {
     console.log("Server running on port " + port)
 })
 
+app.get("/service-worker.js", (req, res) => {
+  res,sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"))
+})
+
 app.post('/payment', (req, res) => {
     const body = {
       source: req.body.token.id,
       amount: req.body.amount,
       currency: 'usd'
-    };
+    }
   
     stripe.charges.create(body, (stripeErr, stripeRes) => {
       if (stripeErr) {
@@ -40,5 +46,5 @@ app.post('/payment', (req, res) => {
       } else {
         res.status(200).send({ success: stripeRes });
       }
-    });
-  });
+    })
+  })
